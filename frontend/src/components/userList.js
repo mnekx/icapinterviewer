9de6462872e98a403de8e2e77f06d57d0607afc9
mnekx <karimnembac@gmail.com>
2024-09-06 07:14:50 +0300

    Remove Git from frontend adn added to main project

---
+++ frontend/src/components/userList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { fetchUsers } from '../services/userService';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await fetchUsers();
+      setUsers(data);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.firstName} {user.lastName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
